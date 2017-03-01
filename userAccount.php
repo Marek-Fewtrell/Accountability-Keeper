cbb3d8585711e0ed77132157d450787cb3f50375
Marek Fewtrell <Marek_Fewtrell@outlook.com.au>
2017-03-01 21:24:55 +1000

    Started adding bootstrap styling to the site. Started with the nav bar and login page. Added ignore file.

--- userAccount.php
+++ userAccount.php
@@ -1,17 +1,16 @@
 <?php
+	session_start();
 	include 'header.html'
 ?>
 <h4>User account</h4>
 <?php
-	session_start();
+	
 	if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] == true) {
 		if (isset($_GET["logout"]) && $_GET['logout'] == 'true') {
 			session_unset();
 			session_destroy();
-		
 			echo "You have been logged out!";
 		} else {
-			
 			echo "User details<br/>";
 			echo "username: " . $_SESSION['username'] . '<br/>';
 			echo "<br/><a href=\"userAccount.php?logout=true\">Logout</a>";
@@ -44,21 +43,46 @@
 			?>
 			Login
 			<br/>
-			<form method="post" action="userAccount.php" >
-				Username: <input type="text" name="username"><br/>
-				Password: <input type="password" name="password"><br/>
-				<input type="submit">
-			</form>
+				<form method="post" action="userAccount.php" class="form-horizontal">
+					<div class="form-group">
+						<label for="username" class="control-label col-sm-2">Username:</label>
+						<div class="col-sm-10">
+							<input type="text" name="username" class="form-control">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password" class="control-label col-sm-2">Password:</label>
+						<div class="col-sm-10">
+							<input type="password" name="password" class="form-control">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<input type="submit" class="btn btn-default">
+						</div>
+					</div>
+				</form>
 			<br/>
 			<br/>
 			Create new account
 			<br/>
 			<form method="post">
 				<input type="hidden" name="createAccount">
-				Username:<input type="text" name="username" value="<?php  ?>"><br/>
-				Password:<input type="password" name="password"><br/>
-				Confirm password:<input type="password" name="passwordConfirm"><br/>
-				<input type="submit">
+				<div class="form-group">
+					<label for="username" class="">Username:</label>
+					<input type="text" name="username" class="form-control" value="<?php  ?>">
+				</div>
+				<div class="form-group">
+					<label for="username" class="">Password:</label>
+					<input type="password" name="password" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="username" class="">Confirm password:</label>
+					<input type="password" name="passwordConfirm" class="form-control">
+				</div>
+				<div class="form-group">
+					<input type="submit" class="btn btn-default">
+				</div>
 			</form>
 			<?php
 		}
